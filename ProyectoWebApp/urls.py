a460c8748c6bdb392f2740d6baa444357fac8de0
Tamara Hidalgo <hidalgotamaraandrea@gmail.com>
2022-08-07 15:13:20 -0300

    nuevo login no aplica, carpeta media/eventos, elimine eventos e hice otros

--- ProyectoWebApp/urls.py
+++ ProyectoWebApp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from ProyectoWebApp import views
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.conf.urls.static import static
 
 urlpatterns = [
    
@@ -14,7 +14,7 @@ urlpatterns = [
     path('blog',views.blog, name="Blog"),
     path('contacto',views.contacto, name="Contacto"),      
 ]
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) #mostrar la foto en el servidor
 
 
 
