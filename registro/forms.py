78d5e7ad49d9c9d9ec168cb7e1afa31f3bb5769f
Tamara Hidalgo <hidalgotamaraandrea@gmail.com>
2022-08-15 17:48:38 -0300

    Di formato al calendario logre hacer el registro pero no va a ningun lado todavia

--- registro/forms.py
+++ registro/forms.py
@@ -1,8 +1,40 @@
 from django import forms
 
-class FormularioRegistro(forms.Form):
-    nombre=forms.CharField(label="Nombre", required=True)
-    email=forms.CharField(label="Email",required=True)
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from usuarios.models import Usuario
+
+class FormularioRegistro(UserCreationForm):
+    class Meta:
+        model = Usuario
+        fields = ["first_name", "last_name", "username", "password1", "password2", "email", "telefono"]
+        labels = {
+            'first_name' : '',
+            'last_name' : '',
+            'username' : '',
+            'password1' : '',
+            'password2' : '',
+            'email' : '',
+            'telefono' : '',
+        }
+
+        widgets = {
+            'first_name' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'Nombre'}),
+            'last_name' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'Apellido'}),
+            'username' : forms.TextInput(attrs={'class':'form-control' ,'placeholder':'Nombre de Usuario'}),
+            'password1' : forms.TextInput(attrs={'class':'form-control' ,'placeholder':'Contraseña'}),
+            'password2' : forms.TextInput(attrs={'class':'form-control' ,'placeholder':'Ingrese su contraseña nuevamente'}),
+            'email' : forms.TextInput(attrs={'class':'form-control' ,'placeholder':'Correo electrónico'}),
+            'telefono' : forms.TextInput(attrs={'class':'form-control' ,'placeholder':'Telefono'}),
+            
+            
+        }
+
+
+
+# class FormularioRegistro(forms.Form):
+#     nombre=forms.CharField(label="Nombre", required=True)
+#     email=forms.CharField(label="Email",required=True)
 
 
     #  fields = ["first_name", "last_name", "username", "password1", "password2", "email", "telefono", "domicilio"]
--- registro/views.py
+++ registro/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render , redirect
 import email
 from django.shortcuts import render
 from .forms import FormularioRegistro
+from usuarios.models import Usuario
+from django.contrib.auth.models import User
 
-# Create your views here.
+
 def login(request):
     formulario_registro = FormularioRegistro
     if request.method=="post":
         formulario_registro = FormularioRegistro(data=request.post)
         if formulario_registro.is_valid():
-            nombre=request.post.get("nombre")
-            email=request.post.get("email")
             return redirect("/registro/?valido")
     return render(request,"registro/login.html",{'miFormulario':formulario_registro})
