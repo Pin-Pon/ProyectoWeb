cfdfc1d7583c8d92afe32894f00d94c7c2a24cc6
Tamara Hidalgo <hidalgotamaraandrea@gmail.com>
2022-08-15 14:07:52 -0300

    modifique el modelo usuario con el atributo es admin

--- usuarios/models.py
+++ usuarios/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class Usuario(AbstractUser):
     telefono =models.CharField(max_length=255,null=True,blank=True) 
+    es_administrador = models.BooleanField(default=False)
 
 
     # def __str__(self):
