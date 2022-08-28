b75a6e1630f4037d0f9c3e3ac1afa77ce22efdab
Tamara Hidalgo <hidalgotamaraandrea@gmail.com>
2022-08-28 12:43:07 -0300

    estoy pasando lo q funciona

--- eventos/models.py
+++ eventos/models.py
@@ -40,5 +40,12 @@ class eventos(models.Model):
         return self.titulo 
 
     def cant_participantes(self):
-        return self.participantes.count()    
+        return self.participantes.count()  
+        
+          
+class CsvFile(models.Model):
+    csv_file = models.FileField(upload_to='documents')  
 
+
+    def __str__(self):
+        return self.csv_file.name   
--- eventos/views.py
+++ eventos/views.py
@@ -1,9 +1,14 @@
 from django.urls import reverse
+from django.shortcuts import render, HttpResponse
+from ProyectoWeb.ProyectoWeb.settings import BASE_DIR
+from ProyectoWeb.eventos.models import CsvFile
 from core.mixins import SuperUsuarioMixin
+import os
+
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from eventos.models import eventos
-from django.views.generic.edit import CreateView , UpdateView , DeleteView
+from django.views.generic import CreateView ,UpdateView , DeleteView
 from .forms import CrearEventoForm
 
 # Create your views here.
@@ -37,7 +42,41 @@ class Eliminar(SuperUsuarioMixin,LoginRequiredMixin, DeleteView):
     
     def get_success_url(self, **kwargs):
         return reverse('EventosNuevos')
+'''
+class CsvUploadView(generic.CreateView):
+    model=CsvFile
+    fields=['csv_file']
+    template_name='eventos/cargar.html'
 
+    def get_queryset(self):
+        return CsvFile.objects.all().order_by('-id')
+
+    def get_success_url(self):
+        return reverse('EventosNuevos')
+
+class CsvDownloadView(generic.ListView):
+    model=CsvFile
+    fields=['csv_file']
+    template_name='eventos/descargar.html'
+
+    def get_queryset(self):
+        return CsvFile.objects.all().order_by('-id')
+
+    def get_success_url(self):
+        return reverse('EventosNuevos')
+'''
+'''
+def desascargarArchivo(request,pk):
+    csv_file = CsvFile.objects.get(pk=pk)
+    filename = csv_file.csv_file.name
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    filepath = BASE_DIR + '/media/' + filename
+    path = open(filepath, 'rb')
+    mime_tipe = mimetypes.guess_type(filepath)
+    response = HttpResponse(path, content_type=mime_tipe)
+    response['Content-Disposition'] = 'attachment; filename=%s' % filename
+    return response
+'''  
 
 
       
